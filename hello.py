512481511e70b8046c8a01368f55b38f9bca6679
jtaren <jatauaren@gmail.com>
2022-11-13 19:11:05 +0100

    initial commit

--- hello.py
+++ hello.py
@@ -1,9 +1,16 @@
 from flask import Flask, render_template
-
+from  flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 # Create a Flask Instance
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "my secret key no one should know"
 
+#Create a Form Class
+class NameForm(FlaskForm):
+    name = StringField("What's Your Name", validators=[DataRequired()])
+    submit = SubmitField("Submit")
 # Craete a route decorator
 @app.route('/')
 
@@ -46,3 +53,15 @@ def page_not_found(e):
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template("500.html"), 500
+
+# Create Name Page
+@app.route('/name', methods=['GET', 'POST'])
+def name():
+    name = None
+    form = NameForm()
+    #Validate Form
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+
+    return render_template("name.html", name=name, form=form)
