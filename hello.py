38bb19c2dac11e6d6d777a7e6a4aa31f3b16f5ed
jtaren <arenjatau@gmail.com>
2024-04-01 17:48:45 +0100

    Update

--- hello.py
+++ hello.py
@@ -264,7 +264,7 @@ def page_not_found(e):
     return render_template("404.html"), 404
 
 # Internal Server Error
-@app.errorhandler(404)
+@app.errorhandler(500)
 def page_not_found(e):
     return render_template("500.html"), 500
 
