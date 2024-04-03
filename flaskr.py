d6eb3c3635c0fc173c1b39043a0d107817e7806b
jtaren <arenjatau@gmail.com>
2024-04-03 11:18:25 +0100

    Update

--- flaskr.py
+++ flaskr.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, flash
+from flask import Flask, render_template, flash, request
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
@@ -26,6 +26,11 @@ class Users(db.Model):
         return '<Name %r>' % self.name
 
 # Create a Form Class
+class UserForm(FlaskForm):
+    name = StringField("Name", validators=[DataRequired()])
+    email = StringField("Email", validators=[DataRequired()])
+    submit = SubmitField("Submit")
+# Create a Form Class
 class NamerForm(FlaskForm):
     name = StringField("What's your Name", validators=[DataRequired()])
     submit = SubmitField("Submit")
@@ -44,6 +49,46 @@ def index():
 @app.route('/user/<name>')
 def user(name):
     return render_template("user.html", user_name=name)
+
+@app.route('/user/add', methods=['GET', 'POST'])
+def add_user():
+    name = None
+    form = UserForm()
+    if form.validate_on_submit():
+        user = Users.query.filter_by(email=form.email.data).first()
+        if user is None:
+            user = Users(name=form.name.data, email=form.email.data)
+            db.session.add(user)
+            db.session.commit()
+        name = form.name.data
+        form.name.data = ''
+        form.email.data = ''
+        flash("User added Successfully")
+    our_users = Users.query.order_by(Users.date_added)
+    return render_template("add_user.html", 
+                           form=form)
+
+# Update Database Record
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
+def update(id):
+    form = UserForm()
+    name_to_update = Users.query.get_or_404(id)
+    if request.method == "POST":
+        name_to_update.name = request.form['name']
+        name_to_update.email = request.form['email']
+        try:
+            db.session.commit()
+            flash("User Updated Successfully")
+            return render_template("update.html",
+                                   form=form,
+                                   name_to_update=name_to_update)
+        except:
+            flash("Error! Looks like there was a problem")
+            return render_template("Update.html", 
+                                   form=form, name_to_update=name_to_update)
+    else:
+        return render_template("update.html", 
+                               form=form, name_to_update=name_to_update)
 
 # Create Custom Error Pages
 #Invalid URL
@@ -67,4 +112,4 @@ def name():
         flash("Form Subitted Successfully")
     return render_template("name.html", 
                            name=name, 
-                           form = form)
+                           form=form)
